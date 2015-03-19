aad23efe0858e0625b299502b144d3678db9f78f
Nikolay Grigoriev <hexgrim@gmail.com>
2015-03-19 03:58:38 +0300

    Считаем все ноды

--- CityRoadSolver/Calculation/Solver.cs
+++ CityRoadSolver/Calculation/Solver.cs
@@ -17,10 +17,41 @@ namespace CityRoadSolver.Calculation
             //TODO: realize calculation
 
             //create nodes
+            nodes = CollectNodes(data);
 
             //force all path variations with cost calculation
 
             return 0f;
+        }
+
+        private IDictionary<string, Node> CollectNodes(Data input)
+        {
+            var collectedNodes = new Dictionary<string, Node>();
+            foreach (var road in input.Roads)
+            {
+                if (collectedNodes.ContainsKey(road.CityNames[0]))
+                {
+                    collectedNodes[road.CityNames[0]].ConnectedNodes.Add(road.CityNames[1], road.FuelCost);
+                }
+                else
+                {
+                    var nod = new Node {Name = road.CityNames[0]};
+                    nod.ConnectedNodes.Add(road.CityNames[1], road.FuelCost);
+                    collectedNodes[road.CityNames[0]] = nod;
+                }
+
+                if (collectedNodes.ContainsKey(road.CityNames[1]))
+                {
+                    collectedNodes[road.CityNames[1]].ConnectedNodes.Add(road.CityNames[0], road.FuelCost);
+                }
+                else
+                {
+                    var nod = new Node { Name = road.CityNames[1] };
+                    nod.ConnectedNodes.Add(road.CityNames[0], road.FuelCost);
+                    collectedNodes[road.CityNames[1]] = nod;
+                }
+            }
+            return collectedNodes;
         }
     }
 }
