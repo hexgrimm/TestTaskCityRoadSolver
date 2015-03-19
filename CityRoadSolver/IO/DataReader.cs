b56aa6dbe5a4c2f93d210403e30ccae88e9daedd
Nikolay Grigoriev <hexgrim@gmail.com>
2015-03-19 03:14:48 +0300

    парс дорог, пара проверок

--- CityRoadSolver/IO/DataReader.cs
+++ CityRoadSolver/IO/DataReader.cs
@@ -1,13 +1,14 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 
 namespace CityRoadSolver.IO
 {
     public sealed class DataReader : IDataReader
     {
-        
         private const string filename = @"\input.txt";
+        private readonly char divider = Char.Parse(" ");
         private string[] lines;
+       
         Data IDataReader.ReadInputData()
         {
             try
@@ -19,7 +20,7 @@ namespace CityRoadSolver.IO
                 Console.WriteLine("Error while reading : " + e.Message);
             }
 
-            if (lines.Length < 2)
+            if (lines == null || lines.Length < 2)
             {
                 Console.WriteLine("Not enough lines in file");
                 return null;
@@ -30,13 +31,40 @@ namespace CityRoadSolver.IO
 
             //resolving target cities
             var targets = lines[0];
-            var divider = Char.Parse(" ");
+           
             var cities = targets.Split(divider);
             answer.TargetCities = cities;
 
             //resolving road infos
+            for (int i = 1; i < lines.Length; i++)
+            {
+                var s = lines[i];
+                if (string.IsNullOrEmpty(s))
+                {
+                    Console.WriteLine("Line '{0}' is not readable at input.txt", i);
+                    continue;
+                }
+
+                answer.Roads.Add(ParseString(lines[i]));
+            }
 
             return answer;
+        }
+
+        private Data.RoadInfo ParseString(string input)
+        {
+            Data.RoadInfo output;
+
+            var rawStrings =  input.Split(divider);
+            if (rawStrings.Length != 3 || rawStrings.Any(string.IsNullOrEmpty))
+            {
+                Console.WriteLine("Line '{0}' is not readable at input.txt", input);
+            }
+
+            output.CityNames = new[] {rawStrings[0], rawStrings[1]};
+            output.FuelCost = int.Parse(rawStrings[2]);
+
+            return output;
         }
     }
 }
--- CityRoadSolver/Program.cs
+++ CityRoadSolver/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CityRoadSolver.IO;
 
 namespace CityRoadSolver
 {
@@ -10,7 +11,9 @@ namespace CityRoadSolver
     {
         static void Main(string[] args)
         {
-
+            IDataReader dataReader = new DataReader();
+            dataReader.ReadInputData();
+            Console.ReadLine();
         }
     }
 }
